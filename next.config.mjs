b4da6f8e4e686c6357f4154606f43bdebca2b9d4
Callum Morrisson <callum.morrisson@uqconnect.edu.au>
2024-10-11 14:24:36 +1000

    common image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,39 @@
-import GameDbGeneratorPlugin from './.build/game-db-generator.plugin.mjs';
+import GameDbGeneratorPlugin from "./.build/game-db-generator.plugin.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
   output: "export",
+  /**
+   * Set base path. environment variable is set in .github/workflows/nextjs.yml
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
     // nextjs has 3 compilers, we only want to attach to the first one
-    if (nextRuntime === 'nodejs') config.plugins.push(new GameDbGeneratorPlugin());
+    if (nextRuntime === "nodejs")
+      config.plugins.push(new GameDbGeneratorPlugin());
     // Important: return the modified config
-    return config
-  }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/games/_components/results/index.tsx
+++ src/app/games/_components/results/index.tsx
@@ -4,6 +4,7 @@ import GameDetailsModel from "@/data/models/game-details.model";
 import { useSearchParams, useSearchResults } from "../search-store";
 
 import style from "./results.module.scss";
+import { GameCoverImage } from "@/components/game-cover-image/game-cover-image";
 
 const DEFAULT_DISPLAYTEMPLATE = {
   grid: (x: GameDetailsModel) => <div>{x.name}</div>,
@@ -19,7 +20,11 @@ export default function Results() {
     <div
       className={`${style["results-container"]} ${style["results-" + display]}`}
     >
-      {results.map((x, i) => <ResultComp key={i} {...x} />)}
+      {results.map((x, i) => <div className={style.item} key={i}>
+          <GameCoverImage gameFolder={x.gameFolder} coverExtension={x.coverExtension} className={style["item-thumbnail"]} />
+          
+          <span className={style["item-name"]}>{x.name}</span>
+        </div>)}
     </div>
   );
 }
