39cdf7162f9e2eae74051cb6b891e33ebe077170
Callum Morrisson <morrisson.callum@gmail.com>
2025-08-14 20:43:27 +1000

    fixing search pagination.

--- src/app/games/_components/pagination/index.tsx
+++ src/app/games/_components/pagination/index.tsx
@@ -15,7 +15,7 @@ export default function Pagination() {
   const [totalResults] = useSearchResults((x) => [
     x.totalResults,
   ]);
-  const [pageNumber, pageSize] = useSearchParams((x) => [ x.pageNumber, x.pageSize ]);
+  const [pageNumber, pageSize] = useSearchParams((x) => [x.pageNumber, x.pageSize]);
   const updateParams = useSearchParamsUpdater();
 
   const numPages = Math.ceil(totalResults / pageSize);
@@ -25,11 +25,11 @@ export default function Pagination() {
   // todo, stop being lazy, this needs thought
   const pages = Array.from({ length: numPages + 1 }, (_, i) =>
     numPages <= 9 || // always show atleast 9
-    i === 1 || // first page
-    i === numPages || // last page
-    (i >= pageNumber - 2 && i <= pageNumber + 2) || // current page and 2 on either side
-    (i === 2 && pageNumber === 5) || // don't show an elipses for a single number
-    (i === numPages - 1 && pageNumber === numPages - 4) // don't show an elipses for a single number
+      i === 1 || // first page
+      i === numPages || // last page
+      (i >= pageNumber - 2 && i <= pageNumber + 2) || // current page and 2 on either side
+      (i === 2 && pageNumber === 5) || // don't show an elipses for a single number
+      (i === numPages - 1 && pageNumber === numPages - 4) // don't show an elipses for a single number
       ? i
       : null
   ).filter((x, i, a) => x || a[i - 1]);
@@ -52,9 +52,8 @@ export default function Pagination() {
             <button
               onClick={() => updateParams({ pageNumber: x })}
               key={i}
-              className={`${styles.page} ${
-                x === pageNumber ? styles["current-page"] : ""
-              }`}
+              className={`${styles.page} ${x === pageNumber ? styles["current-page"] : ""
+                }`}
             >
               {x}
             </button>
@@ -66,7 +65,7 @@ export default function Pagination() {
         )}
         <button
           className={`${styles.page} ${styles["page-next"]}`}
-          disabled={pageNumber === numPages}
+          disabled={pageNumber === numPages || numPages === 0}
           onClick={() => updateParams({ pageNumber: pageNumber + 1 })}
         >
           <IoMdArrowDropright />
